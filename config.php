8d885d7133b0c2e6fdb41bf25c73dc1ffd1002dc
Onkar Yaglewad <110722317+onkar-yaglewad@users.noreply.github.com>
2022-12-14 20:39:41 +0530

    Initial Commit - Making Drip Open Source

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "drip_6215adminuseronkar";
+// MySQL Database Password
+$sql_db_pass = "MpFJbf4Fq0F7j46IK17PMbvn-G2C4lHH";
+// MySQL Database Name
+$sql_db_name = "drip_maindb6215";
+
+// Site URL
+$site_url = "https://drip.sparck.tk"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "56556451v529846ctyhugjb5165vyhgvbucdzssdfghj4956vhjnk6548564dfs"; // Your purchase code, don't give it to anyone.
+?>
